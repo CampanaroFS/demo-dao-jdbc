748d3d2669e43b56e23eb6e730f7efd92c1e6a63
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-04-17 21:55:49 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.SellerDao;
+import model.dao.impl.DaoFactory;
 import model.entities.Department;
 
 public class Program {
@@ -7,6 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(obj);
 	}
 
